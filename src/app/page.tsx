7b9367146737bf3e5291b2b414a008468c354631
kazutoki-nawamaki-lvgs <kazutoki.nawamaki@leverages.jp>
2024-03-21 11:10:25 +0900

    3月20日(水)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,67 @@
 import Link from "next/link";
+import "./globals.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h2>氏名</h2>
+    <div className="font-base">
+      <div className="bg-[url('/malta.jpg')] bg-cover bg-center text-white text-4xl p-auto h-3/4 viewport-80vh align-center flex flex-col justify-center items-center">
+        <div className="w-full flex flex-col items-center justify-center bg-white py-6">
+          <p className="text-6xl mb-4 bg-white text-black px-4">
+            KAZUTOKI NAWAMAKI
+          </p>
+          <p className="bg-white/30 text-black px-4">
+            ENGINEER / DESIGN / PHOTO
+          </p>
+          <p className="bg-white text-black px-4"> PORTFOLIO SITE</p>
+        </div>
+      </div>
+      <div
+        id="profile"
+        className="flex flex-col justify-center items-center text-center min-viewport-50vh"
+      >
+        <p className="text-4xl">PROFILE</p>
+        <Image
+          src="/lucci.png"
+          width={200}
+          height={200}
+          alt="Picture of Kazutoki"
+          className="rounded-full"
+        />
         <p>縄巻一鴻</p>
+        <p>大阪大学</p>
+        <p>2023年11月ごろから駆け出しのエンジニアをしております。</p>
+        <p>
+          このサイトはエンジニアとしての自己研鑽のために作った
+          <br />
+          Webサイト / Webアプリケーションを載せております。
+        </p>
+        <p>
+          またそれとは別に、海外旅行に行った時に撮った写真も掲載しております。
+        </p>
+        <p>
+          今後もアプリケーションやデザインのポートフォリオの内容を追加していく予定ですので、
+          <br />
+          どうぞよろしくお願いいたします。
+        </p>
       </div>
-      <div>
-        <h2>経歴</h2>
+      <div id="skill" className="flex flex-col justify-center items-center">
+        <p className="text-4xl">SKILL</p>
       </div>
-      <div>
-        <h2>自己紹介</h2>
+      <div id="portfolio" className="flex flex-col justify-center items-center">
+        <p className="text-4xl">PORTFOLIO</p>
       </div>
-      <div>
-        <h2>保有資格</h2>
+      <div
+        id="certification"
+        className="flex flex-col justify-center items-center"
+      >
+        <p className="text-4xl">CERTIFICATION</p>
       </div>
-      <div>
-        <h2>連絡先</h2>
-        <Link href="/test">テスト</Link>
+      <div id="photo" className="flex flex-col justify-center items-center">
+        <p className="text-4xl">PHOTO</p>
+      </div>
+      <div id="contact" className="flex flex-col justify-center items-center">
+        <p className="text-4xl">CONTACT</p>
       </div>
     </div>
   );
