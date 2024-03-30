7cfbcdfdc010a2aa98b88eafcd6bc7b6532410da
kazutoki-nawamaki-lvgs <kazutoki.nawamaki@leverages.jp>
2024-03-30 19:17:25 +0900

    2024/03/30-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_HOME_PAGE: "http://localhost:3000/home",
-    NEXT_PUBLIC_PHOTO_PAGE: "http://localhost:3000/photo",
-    NEXT_PUBLIC_MALTA_PAGE: "http://localhost:3000/photo/malta",
-    NEXT_PUBLIC_USA_PAGE: "http://localhost:3000/photo/usa",
+    // prod
+    NEXT_PUBLIC_HOME_PAGE: "https://kazutoki-dev.vercel.app/home",
+    NEXT_PUBLIC_PHOTO_PAGE: "https://kazutoki-dev.vercel.app/photo",
+    NEXT_PUBLIC_MALTA_PAGE: "https://kazutoki-dev.vercel.app/photo/malta",
+    NEXT_PUBLIC_USA_PAGE: "https://kazutoki-dev.vercel.app/photo/usa",
+
+    // dev
+    // NEXT_PUBLIC_HOME_PAGE: "http://localhost:3000/home",
+    // NEXT_PUBLIC_PHOTO_PAGE: "http://localhost:3000/photo",
+    // NEXT_PUBLIC_MALTA_PAGE: "http://localhost:3000/photo/malta",
+    // NEXT_PUBLIC_USA_PAGE: "http://localhost:3000/photo/usa",
   },
 
   async redirects() {
